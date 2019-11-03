36e9a529d5525a84a89ce031591cde439e9ba22d
Atiqur <atiqur.su@gmail.com>
2019-11-03 16:58:28 +0600

    #token column added into the database.

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -4,16 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
-{
+class CreateFailedJobsTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
+
         Schema::create('failed_jobs', function (Blueprint $table) {
+
             $table->bigIncrements('id');
             $table->text('connection');
             $table->text('queue');
@@ -23,13 +24,14 @@ class CreateFailedJobsTable extends Migration
         });
     }
 
+
     /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
+
         Schema::dropIfExists('failed_jobs');
     }
 }
